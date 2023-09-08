bd6cf74f7d2c7d826c8dde431f392a2ee9958aca
umer  baig <umarbaig1998@gmail.com>
2023-09-08 14:14:03 +0500

    create file to export all views

---
+++ views/forms/index.js
@@ -0,0 +1,6 @@
+export { default as Step1Form } from "./propertyDetails";
+export { default as Step2Form } from "./rentRoll";
+export { default as Step3Form } from "./financing";
+export { default as Congratulations } from "./congratulations";
+
+
