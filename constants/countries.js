84d1a483237e6e259d35a29fa65fe9d7931946e0
umer  baig <umarbaig1998@gmail.com>
2023-09-08 14:12:08 +0500

    add data for countries + cities

---
+++ constants/countries.js
@@ -0,0 +1,27 @@
+export const countries = [
+    { value: "US", label: "United States" },
+    { value: "CA", label: "Canada" },
+    { value: "UK", label: "United Kingdom" },
+    { value: "AU", label: "Australia" },
+    { value: "DE", label: "Germany" },
+    { value: "FR", label: "France" },
+    { value: "JP", label: "Japan" },
+    { value: "CN", label: "China" },
+    { value: "BR", label: "Brazil" },
+    { value: "IN", label: "India" }
+];
+
+export const cities = [
+    { value: "NYC", label: "New York City" },
+    { value: "LA", label: "Los Angeles" },
+    { value: "CHI", label: "Chicago" },
+    { value: "HOU", label: "Houston" },
+    { value: "MIA", label: "Miami" },
+    { value: "LON", label: "London" },
+    { value: "PAR", label: "Paris" },
+    { value: "TOK", label: "Tokyo" },
+    { value: "SYD", label: "Sydney" },
+    { value: "MUM", label: "Mumbai" }
+  ];
+  
+  
