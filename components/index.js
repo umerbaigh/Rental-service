8970b8347885f87a6d31ab7abf8188d43489428f
umer  baig <umarbaig1998@gmail.com>
2023-09-08 14:11:02 +0500

    create index file to export all components

---
+++ components/index.js
@@ -0,0 +1,11 @@
+export { default as Stepper } from "./stepper";
+export { default as Input } from "./input";
+export { default as Button } from "./button";
+export { default as Table } from "./table";
+export { default as SelectField } from "./select";
+export { default as TabComp } from "./tabs";
+
+
+
+
+
